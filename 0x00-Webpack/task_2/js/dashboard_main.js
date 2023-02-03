7bc1a62701f32999156be0161f965b0620186311
Grace <graceffiong@gmail.com>
2023-02-03 17:03:32 +0100

    task 2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,19 @@
+const $ = require('jquery');
+const _ = require('lodash');
+
+import '../css/main.css';
+
+let counter = 0;
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append("<p id='count'></p>");
+$('body').append('<p>Copyright - Holberton School</p>');
+
+function updateCounter() {
+    counter++;
+    $('#count').html(`${counter} clicks on the button`);
+  }
+  
+  $("button").on("click", _.debounce(updateCounter, 500));
