6146f32272b0277973d33d4ca8d699b82f648301
Grace <graceffiong@gmail.com>
2023-02-07 14:48:20 +0100

    task 8

--- 0x01-react_intro/task_5/dashboard/src/index.js
+++ 0x01-react_intro/task_5/dashboard/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App/App';
-import Notifications from './Notifications/Notifications';
+import ReactDOM from'react-dom';
+import App from '../src/App/App';
+import Notifications from '../src/Notifications/Notifications';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
